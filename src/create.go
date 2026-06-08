3d570e96eeabf9b030a79c8611ca973e0151da5b
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-08 13:15:44 -0700

    Automatically create eula.txt before jar execution

--- src/create.go
+++ src/create.go
@@ -9,8 +9,17 @@ import (
 func create(world string) {
 	fmt.Println("Creating server...")
 	config := readConfig()
+
+	err := os.WriteFile("eula.txt", []byte("eula=true"), 0644)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("create server.")
+
 	cmd := exec.Command("java", "-jar", config.Jar)
-	err := cmd.Run()
+	err = cmd.Run()
 
 	if err != nil {
 		fmt.Println(err)
