adb31dcbf9c805ecad511ded48cd688e6fabfb2e
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-08 13:05:42 -0700

    Execute using jar file from config.json

--- src/create.go
+++ src/create.go
@@ -7,8 +7,15 @@ import (
 )
 
 func create(world string) {
-	fmt.Println(world)
 	fmt.Println("Creating server...")
-	exec.Command("java", "-jar", "paper.jar")
+	config := readConfig()
+	cmd := exec.Command("java", "-jar", config.Jar)
+	err := cmd.Run()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
 	os.Mkdir(world, 0755)
 }
