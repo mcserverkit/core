e86d8be037c18320bd68d765aa90ead374ee99c1
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-08 15:00:04 -0700

    Rename world parameter to name parameter

--- src/create.go
+++ src/create.go
@@ -6,19 +6,19 @@ import (
 	"os/exec"
 )
 
-func create(world string) {
+func create(name string) {
 	fmt.Println("Creating server...")
-	os.Mkdir(world, 0755)
+	os.Mkdir(name, 0755)
 	config := readConfig()
 
-	err := os.WriteFile(world+"/eula.txt", []byte("eula=true"), 0644)
+	err := os.WriteFile(name+"/eula.txt", []byte("eula=true"), 0644)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
 	cmd := exec.Command("java", "-jar", config.Jar)
-	cmd.Dir = world
+	cmd.Dir = name
 	err = cmd.Run()
 
 	if err != nil {
