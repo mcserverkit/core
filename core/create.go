fb5904f21fa1893e0bbf00fc0d1084baac986563
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-08 21:23:37 -0700

    Return error if error exists for debugging

--- core/create.go
+++ core/create.go
@@ -5,13 +5,17 @@ import (
 	"os"
 )
 
-func Create(name string) {
+func Create(name string) error {
 	fmt.Println("Creating server...")
-	os.Mkdir(name, 0755)
-
-	err := os.WriteFile(name+"/eula.txt", []byte("eula=true"), 0644)
+	err := os.Mkdir(name, 0755)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return err
 	}
+
+	err = os.WriteFile(name+"/eula.txt", []byte("eula=true"), 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
