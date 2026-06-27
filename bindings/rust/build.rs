ea5f034a6a9bc56a230460a90a5cdc8ff44428cf
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-27 15:41:04 -0700

    Add operating system checking logic

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -1,3 +1,9 @@
+// https://stackoverflow.com/questions/43292357/how-can-one-detect-the-os-type-using-rust
 fn main() {
     println!("Hello World!");
+
+    if cfg!(target_os = "windows") {
+    } else if cfg!(target_os = "linux") {
+    } else if cfg!(target_os = "macos") {
+    }
 }
