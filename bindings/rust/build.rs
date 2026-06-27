1e8c9e22f4ddd844c47e1a2fa49355a0dabbbe8d
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-27 16:17:38 -0700

    Setup commands depending on operating system

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -1,9 +1,18 @@
+use std::process::Command;
+
 fn main() {
     println!("Hello World!");
 
     // https://stackoverflow.com/questions/43292357/how-can-one-detect-the-os-type-using-rust
-    if cfg!(target_os = "windows") {
+    // https://doc.rust-lang.org/std/process/struct.Command.html
+    let _output = if cfg!(target_os = "windows") {
+        Command::new("go")
     } else if cfg!(target_os = "linux") {
+        Command::new("go")
     } else if cfg!(target_os = "macos") {
-    }
+        Command::new("go")
+    } else {
+        println!("Operating system not supported");
+        return;
+    };
 }
