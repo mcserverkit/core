9884cc0dbdca7912549525ce20401e8ece0fd7db
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-27 17:20:15 -0700

    Add full build command per operating system

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -6,11 +6,29 @@ fn main() {
     // https://stackoverflow.com/questions/43292357/how-can-one-detect-the-os-type-using-rust
     // https://doc.rust-lang.org/std/process/struct.Command.html
     let _output = if cfg!(target_os = "windows") {
-        Command::new("go")
+        Command::new("go").args([
+            "build",
+            "-buildmode=c-shared",
+            "-o",
+            "mcserverkit.dll",
+            "./bindings/c",
+        ]);
     } else if cfg!(target_os = "linux") {
-        Command::new("go")
+        Command::new("go").args([
+            "build",
+            "-buildmode=c-shared",
+            "-o",
+            "mcserverkit.so",
+            "./bindings/c",
+        ]);
     } else if cfg!(target_os = "macos") {
-        Command::new("go")
+        Command::new("go").args([
+            "build",
+            "-buildmode=c-shared",
+            "-o",
+            "mcserverkit.dylib",
+            "./bindings/c",
+        ]);
     } else {
         println!("Operating system not supported");
         return;
