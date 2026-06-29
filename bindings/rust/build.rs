05f283cf5798c36cc18c0438a8ad435508c85c6c
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-29 15:52:57 -0700

    Create architecture variable and find filename depending on CPU architecture

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -1,11 +1,18 @@
 fn main() {
+    let architecture = if cfg!(target_arch = "x86_64") {
+        "x86_64"
+    } else if cfg!(target_arch = "aarch64") {
+        "arm64"
+    } else {
+        return;
+    };
     // https://stackoverflow.com/questions/43292357/how-can-one-detect-the-os-type-using-rust
     let filename = if cfg!(target_os = "windows") {
-        "mcserverkit.windows-x86_64.zip"
+        format!("mcserverkit.windows-{architecture}.zip")
     } else if cfg!(target_os = "linux") {
-        "mcserverkit.linux-x86_64.zip"
+        format!("mcserverkit.linux-{architecture}.zip")
     } else if cfg!(target_os = "macos") {
-        "mcserverkit.macos-x86_64.zip"
+        format!("mcserverkit.macos-{architecture}.zip")
     } else {
         println!("Operating system not supported");
         return;
