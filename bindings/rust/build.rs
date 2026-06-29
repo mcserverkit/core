524d72501060136aee48d70ffbde0ba87e782bc3
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-29 13:25:05 -0700

    Add filename variable depending on operating system

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -2,11 +2,12 @@ fn main() {
     println!("Hello World!");
 
     // https://stackoverflow.com/questions/43292357/how-can-one-detect-the-os-type-using-rust
-    let _output = if cfg!(any(
-        target_os = "windows",
-        target_os = "linux",
-        target_os = "macos"
-    )) {
+    let _output = if cfg!(target_os = "windows") {
+        let filename = "mcserverkit.windows-x86_64.zip";
+    } else if cfg!(target_os = "linux") {
+        let filename = "mcserverkit.linux-x86_64.zip";
+    } else if cfg!(target_os = "macos") {
+        let filename = "mcserverkit.macos-x86_64.zip";
     } else {
         println!("Operating system not supported");
         return;
