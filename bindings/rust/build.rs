dbc4b7e5d3f5ff707fe25d65bd8b0c504fe755e4
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-29 16:09:32 -0700

    Create url variable

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -19,6 +19,8 @@ fn main() {
     };
 
     let filename = format!("mcserverkit.{os}-{architecture}.zip");
+    let url =
+        format!("https://github.com/mcserverkit/mcserverkit/releases/latest/download/{filename}");
 
-    println!("{filename}");
+    println!("{url}");
 }
