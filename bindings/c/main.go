1e1cf7ff71ee9015947442ebf4fd238004b7426b
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-07-02 15:00:07 -0700

    Return CString instead of Integers for debugging

--- bindings/c/main.go
+++ bindings/c/main.go
@@ -10,30 +10,30 @@ import (
 )
 
 //export Install
-func Install(version *C.char) C.int {
+func Install(version *C.char) *C.char {
 	err := mcserverkit.Install(C.GoString(version))
 	if err != nil {
-		return 1
+		return C.CString(err.Error())
 	}
-	return 0
+	return nil
 }
 
 //export Create
-func Create(name *C.char, eula C.bool) C.int {
+func Create(name *C.char, eula C.bool) *C.char {
 	err := mcserverkit.Create(C.GoString(name), bool(eula))
 	if err != nil {
-		return 1
+		return C.CString(err.Error())
 	}
-	return 0
+	return nil
 }
 
 //export Start
-func Start(name *C.char, memory *C.char) C.int {
+func Start(name *C.char, memory *C.char) *C.char {
 	err := mcserverkit.Start(C.GoString(name), C.GoString(memory))
 	if err != nil {
-		return 1
+		return C.CString(err.Error())
 	}
-	return 0
+	return nil
 }
 
 func main() {}
