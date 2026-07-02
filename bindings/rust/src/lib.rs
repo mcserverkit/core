86b0f84330dc70d07c54ee3ffed2c1d03dc337c8
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-07-02 16:18:18 -0700

    Setup error handling and return error message

--- bindings/rust/src/lib.rs
+++ bindings/rust/src/lib.rs
@@ -1,23 +1,44 @@
 use std::ffi;
 
 unsafe extern "C" {
-    fn Install(version: *const ffi::c_char);
-    fn Create(name: *const ffi::c_char, eula: bool);
-    fn Start(name: *const ffi::c_char, memory: *const ffi::c_char);
+    fn Install(version: *const ffi::c_char) -> *const ffi::c_char;
+    fn Create(name: *const ffi::c_char, eula: bool) -> *const ffi::c_char;
+    fn Start(name: *const ffi::c_char, memory: *const ffi::c_char) -> *const ffi::c_char;
 }
 
-pub fn install(version: &str) {
+pub fn install(version: &str) -> Option<String> {
     let version = ffi::CString::new(version).expect("");
-    unsafe { Install(version.as_ptr()) }
+    unsafe {
+        let err = Install(version.as_ptr());
+        if err.is_null() {
+            None
+        } else {
+            Some(ffi::CStr::from_ptr(err).to_str().unwrap().to_string())
+        }
+    }
 }
 
-pub fn create(name: &str, eula: bool) {
+pub fn create(name: &str, eula: bool) -> Option<String> {
     let name = ffi::CString::new(name).expect("");
-    unsafe { Create(name.as_ptr(), eula) }
+    unsafe {
+        let err = Create(name.as_ptr(), eula);
+        if err.is_null() {
+            None
+        } else {
+            Some(ffi::CStr::from_ptr(err).to_str().unwrap().to_string())
+        }
+    }
 }
 
-pub fn start(name: &str, memory: &str) {
+pub fn start(name: &str, memory: &str) -> Option<String> {
     let name = ffi::CString::new(name).expect("");
     let memory = ffi::CString::new(memory).expect("");
-    unsafe { Start(name.as_ptr(), memory.as_ptr()) }
+    unsafe {
+        let err = Start(name.as_ptr(), memory.as_ptr());
+        if err.is_null() {
+            None
+        } else {
+            Some(ffi::CStr::from_ptr(err).to_str().unwrap().to_string())
+        }
+    }
 }
